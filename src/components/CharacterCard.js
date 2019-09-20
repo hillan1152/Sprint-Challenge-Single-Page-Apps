3bc50482039a92177e7331382d64f5f262c21178
Anthony Hill <hillan1152@gmail.com>
2019-09-20 12:58:55 -0400

    Got a name up

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+// import styled from "styled-components";
+
+
+
+const CharacterCard = props => {
+  return (
+    <div key={props.id}>
+      <h1>{props.name}</h1>
+    </div>
+  )
 }
+export default CharacterCard
+
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    const getCharacters = () => {
-      axios
-      .get("https://rickandmortyapi.com/api/character/")
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array! 
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
       .then(res => {
-        setCharacters(res.data);
+        setCharacters(res.data.results);
+        console.log(res.data.results);
       })
       .catch(error => {
         console.log("There is an error", error)
       })
-    }
-    getCharacters();
-  }, [characters]);
+
+  }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-      {/* {characters.map(character => (
-
-      ))} */}
+      {characters.map(character => {
+        return <h1>{character.name}</h1>
+      })}
     </section>
   );
 }
